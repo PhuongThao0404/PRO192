ed5987137f5816ae13d53a0fdab2a8fff507ce5c
Your Name <you@example.com>
2023-10-23 13:41:41 +0700

    Bai8

--- Baitap/Bai8/src/giang_vien/GV.java
+++ Baitap/Bai8/src/giang_vien/GV.java
@@ -1,13 +1,14 @@
-package person;
+package giang_vien;
 import java.util.Scanner;
-class person1 {
-	 private String email;
+class person1{
+		
+	private String email;
 	 private String full_name;
 	 private String date_of_birth;
 	 private String sex;
 	 public String phone_number;
 	 public String Address;
-	 public void getInformation() {
+	 public void getInformation1() {
 		 Scanner sc = new Scanner(System.in);
 		 System.out.println("Name: ");
 		 full_name = sc.nextLine();
@@ -22,20 +23,33 @@ class person1 {
 		 System.out.println("Address: ");
 		 Address = sc.nextLine();
 		 
-	 }public void display() {
+	 }public void display1() {
 		 System.out.println(full_name);
 		 System.out.println(email);
 		 System.out.println(date_of_birth);
 		 System.out.println(sex);
 		 System.out.println(phone_number);
 		 System.out.println(Address);
+		 
 	 }
-
-} public class Person{
-	 public static void main(String[] args)
-	 {
-		 person1 p1= new person1();
-		 p1.getInformation();
-		 p1.display();
-	 }
-}
+	}class giang_vien extends person1{
+		public String danhsach;
+		public String hocphan;
+		public void getInformation() {
+			Scanner gv = new Scanner(System.in);
+			System.out.println("Mon hoc se day: ");
+			danhsach = gv.nextLine();}
+	    public void display () {
+	    	 System.out.println(danhsach);
+	    }
+		}
+	public class GV{
+		public static void main (String[] args) {
+			giang_vien gv = new giang_vien();
+			gv.getInformation1();
+			gv.getInformation();
+			gv.display1();
+			gv.display();
+			
+		}
+	}
